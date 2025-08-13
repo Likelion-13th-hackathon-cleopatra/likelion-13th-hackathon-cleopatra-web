526f694409ed238fbc43847e7508055e0ebe9215
홍석주 <hsj202201@gmail.com>
2025-08-13 15:39:46 +0900

    feat. vercel 배포 제발..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',
+        'sm': '375px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+      maxWidth: {
+        'mobile': '430px',
+      },
+      width: {
+        'mobile': '430px',
+      },
+    },
   },
   plugins: [],
 } 
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const svgr = require("vite-plugin-svgr");
+
+module.exports = defineConfig({
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+}); 
