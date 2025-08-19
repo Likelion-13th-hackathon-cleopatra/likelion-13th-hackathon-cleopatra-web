b9d2b4abbeab7956575dd298f5affec062faafd5
홍석주 <hsj202201@gmail.com>
2025-08-19 19:11:07 +0900

    feat. 컬러 팔레트 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,27 +39,38 @@ export default {
       },
 
       colors: {
-        "Primary_Green": "#0DB659",
-        "Sub_Yellow": "#FFC251",
-        "Grayscale_White": "#FFF",
+        'primary-green': '#0DB659',
+        'sub-yellow': '#FFC251',
+        'grayscale-white': '#ffffff',
+        'sub-brown': '#A87E3A',
       },
-      // 그라데이션(오버레이) 목록
       backgroundImage: {
-        "Primary_Green_15": "linear-gradient(0deg, rgba(0, 0, 0, 0.15) 0%, rgba(0, 0, 0, 0.15) 100%), #0DB659",
-        "Primary_Green_40": "linear-gradient(0deg, rgba(0, 0, 0, 0.40) 0%, rgba(0, 0, 0, 0.40) 100%), #0DB659",
-        "Primary_Green_60": "linear-gradient(0deg, rgba(0, 0, 0, 0.20) 0%, rgba(0, 0, 0, 0.20) 100%), linear-gradient(0deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 100%), #0DB659",
-        "Primary_Green_80": "linear-gradient(0deg, rgba(0, 0, 0, 0.80) 0%, rgba(0, 0, 0, 0.80) 100%), #0DB659",
-        "Sub_Yellow_40": "linear-gradient(0deg, rgba(0, 0, 0, 0.40) 0%, rgba(0, 0, 0, 0.40) 100%), #FFC251",
-        "Sub_Yellow_60": "linear-gradient(0deg, rgba(0, 0, 0, 0.60) 0%, rgba(0, 0, 0, 0.60) 100%), #FFC251",
-        "Grayscale_5": "linear-gradient(0deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.00) 100%), #F3F3F1",
-        "Grayscale_15": "linear-gradient(0deg, rgba(0, 0, 0, 0.15) 0%, rgba(0, 0, 0, 0.15) 100%), #F3F3F1",
-        "Grayscale_25": "linear-gradient(0deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%), #F3F3F1",
-        "Grayscale_45": "linear-gradient(0deg, rgba(0, 0, 0, 0.45) 0%, rgba(0, 0, 0, 0.45) 100%), #F3F3F1",
-        "Grayscale_65": "linear-gradient(0deg, rgba(0, 0, 0, 0.65) 0%, rgba(0, 0, 0, 0.65) 100%), #F3F3F1",
-        "Grayscale_85": "linear-gradient(0deg, rgba(0, 0, 0, 0.85) 0%, rgba(0, 0, 0, 0.85) 100%), #F3F3F1",
-
-
-        // 필요하면 더 추가
+        'primary-green-15':
+          'linear-gradient(0deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.15) 100%), #0DB659',
+        'primary-green-40':
+          'linear-gradient(0deg, rgba(0,0,0,0.40) 0%, rgba(0,0,0,0.40) 100%), #0DB659',
+        'primary-green-60':
+          'linear-gradient(0deg, rgba(0,0,0,0.20) 0%, rgba(0,0,0,0.20) 100%), linear-gradient(0deg, rgba(0,0,0,0.60) 0%, rgba(0,0,0,0.60) 100%), #0DB659',
+        'primary-green-80':
+          'linear-gradient(0deg, rgba(0,0,0,0.80) 0%, rgba(0,0,0,0.80) 100%), #0DB659',
+    
+        'sub-yellow-40':
+          'linear-gradient(0deg, rgba(0,0,0,0.40) 0%, rgba(0,0,0,0.40) 100%), #FFC251',
+        'sub-yellow-60':
+          'linear-gradient(0deg, rgba(0,0,0,0.60) 0%, rgba(0,0,0,0.60) 100%), #FFC251',
+    
+        'grayscale-5':
+          'linear-gradient(0deg, rgba(0,0,0,0.00) 0%, rgba(0,0,0,0.00) 100%), #F3F3F1',
+        'grayscale-15':
+          'linear-gradient(0deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.15) 100%), #F3F3F1',
+        'grayscale-25':
+          'linear-gradient(0deg, rgba(0,0,0,0.25) 0%, rgba(0,0,0,0.25) 100%), #F3F3F1',
+        'grayscale-45':
+          'linear-gradient(0deg, rgba(0,0,0,0.45) 0%, rgba(0,0,0,0.45) 100%), #F3F3F1',
+        'grayscale-65':
+          'linear-gradient(0deg, rgba(0,0,0,0.65) 0%, rgba(0,0,0,0.65) 100%), #F3F3F1',
+        'grayscale-85':
+          'linear-gradient(0deg, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0.85) 100%), #F3F3F1',
       },
     },
   },
