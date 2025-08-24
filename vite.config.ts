e2b35a7a19d22de7fa55099580471cffd1f04b78
홍석주 <hsj202201@gmail.com>
2025-08-25 00:17:04 +0900

    feat. svg 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import svgr from "vite-plugin-svgr";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(), 
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -16,5 +23,9 @@ export default defineConfig({
         manualChunks: undefined,
       },
     },
+    assetsInlineLimit: 0, // SVG 파일을 인라인하지 않음
+  },
+  optimizeDeps: {
+    include: ['**/*.svg?react'], // SVG React 컴포넌트를 의존성에 포함
   },
 });
